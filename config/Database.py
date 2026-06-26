b86508b134c010229e605f8200a40eafc1ef7b45
karamjit-2660 <37602@esjaloures.org>
2026-06-26 13:59:32 +0100

    Alterei mensagens que tava dar erro

--- config/Database.py
+++ config/Database.py
@@ -2,7 +2,7 @@ import mysql.connector
 from mysql.connector import Error
 
 DB_CONFIG = {
-    "host": "192.168.1.21",
+    "host": "192.168.1.100",
     "port": 3306,
     "user": "root",
     "password": "",
